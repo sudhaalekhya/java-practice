c26b160b66c521c74bb15392e60d22a7f90ecde7
sudhaalekhya <reddyalekhya394@gmail.com>
2024-06-07 15:48:44 +0530

    Add files via upload

---
+++ UDF1.java
@@ -0,0 +1,20 @@
+package functions;
+import java.util.Scanner;
+public class UDF1 {
+	public void add()
+	{
+		int a,b,c;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter 2 numbers:");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		c=a+b;
+		System.out.println(a+"+"+b+"="+c);
+	}
+	public static void main(String[] args) {
+		 UDF1 ob=new  UDF1();
+	       ob.add();	
+
+	}
+
+}
---
+++ UDF3.java
@@ -0,0 +1,21 @@
+package functions;
+import java.util.Scanner;
+public class UDF3 {
+	public int add()
+	   {
+		    int a,b,c;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter 2 numbers:");
+			a=sc.nextInt();
+			b=sc.nextInt();
+			c=a+b;
+			return c;
+	   }
+	public static void main(String[] args) {
+		UDF3 ob=new  UDF3();
+	    int z=ob.add();
+	    System.out.println(z);
+
+	}
+
+}
